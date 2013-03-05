c847a1f47c578231d9e83b4bf7442c9f8ed89659
vicchi <gary@vicchi.org>
2013-03-05 10:45:47 +0000

    Fix colours to explicitly use # notation. Chrome/Safari are tolerant of a lack of #, FF is not

--- js/great-british-map-nav.js
+++ js/great-british-map-nav.js
@@ -1,205 +1,205 @@
 $(document).ready(function() {
 	$('#group-bi').mouseenter(function() {
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.IRL').css({'fill' : 'CCCCCC'});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.IRL').css({'fill' : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
 
-		$('.subunit.GGA').css({"fill" : "FFFFFF"});
-		$('.subunit.GGG').css({"fill" : "FFFFFF"});
-		$('.subunit.GGS').css({"fill" : "FFFFFF"});
-		$('.subunit.JEY').css({"fill" : "FFFFFF"});
+		$('.subunit.GGA').css({"fill" : "#FFFFFF"});
+		$('.subunit.GGG').css({"fill" : "#FFFFFF"});
+		$('.subunit.GGS').css({"fill" : "#FFFFFF"});
+		$('.subunit.JEY').css({"fill" : "#FFFFFF"});
 		return false;
 	});
 
 	$('#island-gbr').mouseenter(function() {
-		$('.subunit.ENG').css({"fill" : "CCFFCC"});
-		$('.subunit.SCT').css({"fill" : "CCFFCC"});
-		$('.subunit.WLS').css({"fill" : "CCFFCC"});
+		$('.subunit.ENG').css({"fill" : "#CCFFCC"});
+		$('.subunit.SCT').css({"fill" : "#CCFFCC"});
+		$('.subunit.WLS').css({"fill" : "#CCFFCC"});
 		
-		$('.subunit.IRL').css({'fill' : 'CCCCCC'});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({'fill' : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		return false;
 	});
 
 	$('#island-irl').mouseenter(function() {
-		$('.subunit.IRL').css({'fill' : 'CCFFCC'});
-		$('.subunit.NIR').css({"fill" : "CCFFCC"});
+		$('.subunit.IRL').css({'fill' : "#CCFFCC"});
+		$('.subunit.NIR').css({"fill" : "#CCFFCC"});
 
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		return false;
 	});
 
 	$('#island-imn').mouseenter(function() {
-		$('.subunit.IMN').css({'fill' : 'CCFFCC'});
+		$('.subunit.IMN').css({'fill' : "#CCFFCC"});
 
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
 
 	$('#island-chl').mouseenter(function() {
-		$('.subunit.GGA').css({"fill" : "CCFFCC"});
-		$('.subunit.GGG').css({"fill" : "CCFFCC"});
-		$('.subunit.GGS').css({"fill" : "CCFFCC"});
-		$('.subunit.JEY').css({"fill" : "CCFFCC"});
+		$('.subunit.GGA').css({"fill" : "#CCFFCC"});
+		$('.subunit.GGG').css({"fill" : "#CCFFCC"});
+		$('.subunit.GGS').css({"fill" : "#CCFFCC"});
+		$('.subunit.JEY').css({"fill" : "#CCFFCC"});
 
-		$('.subunit.IMN').css({'fill' : 'CCCCCC'});
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
+		$('.subunit.IMN').css({'fill' : "#CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
 
 	$('#state-gbr').mouseenter(function() {
-		$('.subunit.ENG').css({"fill" : "CC3333"});
-		$('.subunit.SCT').css({"fill" : "CC3333"});
-		$('.subunit.WLS').css({"fill" : "CC3333"});
-		$('.subunit.NIR').css({"fill" : "CC3333"});
+		$('.subunit.ENG').css({"fill" : "#CC3333"});
+		$('.subunit.SCT').css({"fill" : "#CC3333"});
+		$('.subunit.WLS').css({"fill" : "#CC3333"});
+		$('.subunit.NIR').css({"fill" : "#CC3333"});
 
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
 
 	$('#state-irl').mouseenter(function() {
-		$('.subunit.IRL').css({"fill" : "CC3333"});
+		$('.subunit.IRL').css({"fill" : "#CC3333"});
 
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
 
 	$('#country-eng').mouseenter(function() {
-		$('.subunit.ENG').css({"fill" : "FFFF99"});
+		$('.subunit.ENG').css({"fill" : "#FFFF99"});
 
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		
 		return false;
 	});
 
 	$('#country-sct').mouseenter(function() {
-		$('.subunit.SCT').css({"fill" : "FFFF99"});
+		$('.subunit.SCT').css({"fill" : "#FFFF99"});
 
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		
 		return false;
 	});
 
 	$('#country-wls').mouseenter(function() {
-		$('.subunit.WLS').css({"fill" : "FFFF99"});
+		$('.subunit.WLS').css({"fill" : "#FFFF99"});
 
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		
 		return false;
 	});
 
 	$('#country-nir').mouseenter(function() {
-		$('.subunit.NIR').css({"fill" : "FFFF99"});
+		$('.subunit.NIR').css({"fill" : "#FFFF99"});
 
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.IMN').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.IMN').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 		
 		return false;
 	});
 
 	$('#cd-imn').mouseenter(function() {
-		$('.subunit.IMN').css({'fill' : '9999FF'});
+		$('.subunit.IMN').css({'fill' : "#9999FF"});
 
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
-		$('.subunit.GGA').css({"fill" : "CCCCCC"});
-		$('.subunit.GGG').css({"fill" : "CCCCCC"});
-		$('.subunit.GGS').css({"fill" : "CCCCCC"});
-		$('.subunit.JEY').css({"fill" : "CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGA').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGG').css({"fill" : "#CCCCCC"});
+		$('.subunit.GGS').css({"fill" : "#CCCCCC"});
+		$('.subunit.JEY').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
 
 	$('#cd-chl').mouseenter(function() {
-		$('.subunit.GGA').css({"fill" : "9999FF"});
-		$('.subunit.GGG').css({"fill" : "9999FF"});
-		$('.subunit.GGS').css({"fill" : "9999FF"});
-		$('.subunit.JEY').css({"fill" : "9999FF"});
+		$('.subunit.GGA').css({"fill" : "#9999FF"});
+		$('.subunit.GGG').css({"fill" : "#9999FF"});
+		$('.subunit.GGS').css({"fill" : "#9999FF"});
+		$('.subunit.JEY').css({"fill" : "#9999FF"});
 
-		$('.subunit.IMN').css({'fill' : 'CCCCCC'});
-		$('.subunit.ENG').css({"fill" : "CCCCCC"});
-		$('.subunit.SCT').css({"fill" : "CCCCCC"});
-		$('.subunit.WLS').css({"fill" : "CCCCCC"});
-		$('.subunit.NIR').css({"fill" : "CCCCCC"});
-		$('.subunit.IRL').css({"fill" : "CCCCCC"});
+		$('.subunit.IMN').css({'fill' : "#CCCCCC"});
+		$('.subunit.ENG').css({"fill" : "#CCCCCC"});
+		$('.subunit.SCT').css({"fill" : "#CCCCCC"});
+		$('.subunit.WLS').css({"fill" : "#CCCCCC"});
+		$('.subunit.NIR').css({"fill" : "#CCCCCC"});
+		$('.subunit.IRL').css({"fill" : "#CCCCCC"});
 
 		return false;
 	});
